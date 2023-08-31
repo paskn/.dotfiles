29e9d5a61c7d59829db3580f7219a850df843b08
paskn <pashakhin@gmail.com>
2023-08-31 16:03:31 +0200

    Center windows when appropriate

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -752,6 +752,10 @@ targets."
   :straight nil
   :config
  (winner-mode 1))
+(use-package centered-window
+  :straight t
+  :config
+  (centered-window-mode))
 
 (use-package rainbow-delimiters
   :straight t
