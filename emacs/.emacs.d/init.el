046db425ca1216de7a729a4f414a357a66eb3f13
paskn <pashakhin@gmail.com>
2024-01-12 19:55:46 +0100

    Revert "huge new year blob of changes: dear future me, i am sorry"
    This reverts commit a3bfb6785fcafa9bd26336eed887ad8c6f4f85e7.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -267,10 +267,10 @@ group by projectile projects.")
 ;; INSTALL PACKAGES
 ;; --------------------------------------
 
-;; ;; adjust window-size to make the current windo larger
-;; (use-package golden-ratio
-;;   :straight t
-;;   :config (golden-ratio-mode 1))
+;; adjust window-size to make the current windo larger
+(use-package golden-ratio
+  :straight t
+  :config (golden-ratio-mode 1))
 
 (use-package mwim
   :straight t
@@ -402,18 +402,18 @@ group by projectile projects.")
 ;;   (doom-themes-org-config))
 
 (use-package modus-themes
-  ;; :defer t
+  :defer t
   :straight t
-  :config
-  (load-theme 'modus-operandi :no-confirm)
+  ;; :config
+  ;; (load-theme 'modus-vivendi :no-confirm))
   )
 
-;; (use-package ef-themes
-;;   ;; :defer t
-;;   :straight (ef-themes :type git :host github :repo "protesilaos/ef-themes")
-;;   :config
-;;   ;; (ef-themes-select 'ef-elea-dark)
-;;   (load-theme 'ef-maris-light t))
+(use-package ef-themes
+  ;; :defer t
+  :straight (ef-themes :type git :host github :repo "protesilaos/ef-themes")
+  :config
+  ;; (ef-themes-select 'ef-elea-dark)
+  (load-theme 'ef-maris-light t))
 
 (use-package spacious-padding
   :straight t
@@ -433,8 +433,6 @@ group by projectile projects.")
 (use-package dashboard
   :straight t
   :init
-  ;; (setq dashboard-startup-banner "~/Downloads/caduceus-0.png")
-  (setq dashboard-startup-banner "~/path1923-9.png")
   (defun sp/generate-strats ()
     (let (result)
       (dotimes (i 5 result)
@@ -468,26 +466,6 @@ group by projectile projects.")
 (use-package emacs
   :straight nil
   :config
-  (when (display-graphic-p)
-    (context-menu-mode))
-  (setq-default use-file-dialog nil)
-  (setq-default use-dialog-box nil)
-  ;; better defaults for completions
-  (setq completions-max-height 20)
-  (setq completions-format 'one-column)
-  (setq completions-group t)
-  (setq completion-auto-select 'second-tab)
-
-  (keymap-set minibuffer-mode-map "TAB" 'minibuffer-complete) ; TAB acts
-                                        ; more
-                                        ; like how
-                                        ; it does
-                                        ; in the
-                                        ; shell
-
-  (setq x-underline-at-descent-line nil)           ; Prettier underlines
-  (setq switch-to-buffer-obey-display-actions t)   ; Make switching buffers more consistent
-  (setq-default show-trailing-whitespace nil)      ; By default, don't underline trailing spaces
   (pixel-scroll-precision-mode 1)
   (setq show-paren-delay 0)
   (setq-default scroll-preserve-screen-position t)
@@ -580,8 +558,6 @@ group by projectile projects.")
   ;; expression that matches any character between the words you give
   ;; it.
   (setq search-whitespace-regexp ".*?")
-  ;; when directions is changed, jump straight to the next result
-  (setq isearch-repeat-on-direction-change t)
   )
 
 ;; (use-package aweshell
@@ -595,7 +571,7 @@ group by projectile projects.")
   ;; Replace bindings. Lazily loaded due by `use-package'.
   :bind (;; C-c bindings (mode-specific-map)
          ("C-c h" . consult-history)
-         ;; ("C-c m" . consult-mode-command)
+         ("C-c m" . consult-mode-command)
          ("C-c k" . consult-kmacro)
          ;; C-x bindings (ctl-x-map)
          ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
@@ -1086,8 +1062,6 @@ targets."
           "pdflatex --shell-escape -interaction nonstopmode -output-directory %o %f"))
   (org-agenda-include-diary 1)
   :config
-  (add-to-list 'load-path "/Users/sp/org/ox-koma-letter.el")
-  (eval-after-load 'ox '(require 'ox-koma-letter))
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((emacs-lisp . t)
@@ -1539,30 +1513,8 @@ DIR must include a .project file to be considered a project."
   :defer t
   :config
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1
-        magit-diff-hide-trailing-cr-characters t
-        ;; to make use of abridge-diff
-        magit-diff-refine-hunk 'all)
-  )
-
-;; help diffing prose
-(use-package abridge-diff
-  :after magit ;; optional, if you'd like to use with magit
-  :init (abridge-diff-mode 1))
-
-;; Make ediff less weird
-(use-package ediff
-  :straight (:type built-in)
-  :config
-  (setq ediff-split-window-function 'split-window-horizontally)
-  (setq ediff-window-setup-function 'ediff-setup-windows-plain))
-
-(use-package treesit-auto
-  :straight t
-  :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
+        magit-diff-hide-trailing-cr-characters t)
+)
 
 ;; R and S-family languages
 (use-package ess
@@ -1571,12 +1523,12 @@ DIR must include a .project file to be considered a project."
   :init
   (require 'ess-site)
   :bind (:map ess-r-mode-map
-          ("M-p" . my/add-pipe)
+          ("M-P" . my/add-pipe)
      :map inferior-ess-r-mode-map
      ("M-P" . " |> "))
   :config
-  ;; (with-eval-after-load 'eglot
-  ;;   (setq completion-category-defaults nil))
+  (with-eval-after-load 'eglot
+    (setq completion-category-defaults nil))
   ;; enable skeleton-pair insert globally
   (setq skeleton-pair t)
   ;;(setq skeleton-pair-on-word t)
@@ -1639,9 +1591,7 @@ DIR must include a .project file to be considered a project."
                  (slot . 1)
                  (window-width . 0.5)
                  (reusable-frames . nil))))
-   (ess-set-style 'RStudio)
-   (setq ess-offset-arguments 'prev-line)
-   )
+   (setq ess-style 'Rstudio))
 
 ;; help with RMarkdown
 (use-package poly-markdown
@@ -1706,8 +1656,6 @@ DIR must include a .project file to be considered a project."
   (setq major-mode-remap-alist
       '((python-mode . python-ts-mode)))
   :config
-  (setq python-shell-interpreter "ipython3")
-  (setq python-shell-interpreter-args "-i --simple-prompt --pprint")
   ;; (add-hook 'python-mode-hook 'company-mode)
   (add-hook 'python-mode-hook #'yas-minor-mode)
   (add-hook 'python-mode-hook 'eglot-ensure)
@@ -1754,13 +1702,6 @@ DIR must include a .project file to be considered a project."
   (setq-default pdf-view-display-size 'fit-width)
   (setq pdf-annot-activate-created-annotations t)
   :hook ((pdf-view-mode-hook . pdf-tools-enable-minor-modes)))
-
-(use-package pdf-view-restore
-  :straight t
-  :after pdf-tools
-  :config
-  (add-hook 'pdf-view-mode-hook 'pdf-view-restore-mode)
-  (setq pdf-view-restore-filename "~/.emacs.d/.pdf-view-restore"))
 
 ;; Epub support
 (use-package nov
@@ -1832,7 +1773,7 @@ DIR must include a .project file to be considered a project."
   :custom
   (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
 
-(use-package hippie
+(use-package hippie-expand
   :straight (:type built-in)
   :custom
   (hippie-expand-try-functions-list
@@ -1879,15 +1820,13 @@ DIR must include a .project file to be considered a project."
          ("C-c p ^" . cape-tex)
          ("C-c p &" . cape-sgml)
          ("C-c p r" . cape-rfc1345))
-  :hook ;; (
-;; (ess-r-mode . sp/cape-capf-setup-ess)
-;;      (inferior-ess-mode . sp/cape-capf-setup-ess)
-;;      (ess-mode . sp/cape-capf-setup-ess)
-;;      (ess-roxy-mode . sp/cape-capf-setup-ess)
-;;      (ess-mode . sp/cape-capf-setup-ess)
-;;      ;; (Poly-Markdown+R . sp/cape-capf-setup-ess)
-;;      ;; (eglot-managed-mode-hook . sp/cape-capf-setup-ess)
-;; )
+  :hook ((ess-r-mode . sp/cape-capf-setup-ess)
+     (inferior-ess-mode . sp/cape-capf-setup-ess)
+     (ess-mode . sp/cape-capf-setup-ess)
+     (ess-roxy-mode . sp/cape-capf-setup-ess)
+     (ess-mode . sp/cape-capf-setup-ess)
+     (Poly-Markdown+R . sp/cape-capf-setup-ess)
+     (eglot-managed-mode-hook . sp/cape-capf-setup-ess))
   :init
   ;; Add `completion-at-point-functions', used by `completion-at-point'.
   (add-to-list 'completion-at-point-functions #'cape-file)
@@ -1909,14 +1848,14 @@ DIR must include a .project file to be considered a project."
  (defun sp/cape-capf-setup-ess ()
    (setq-local completion-at-point-functions
            (list (cape-super-capf
-              ;; (cape-company-to-capf #'company-R-objects)
-              ;; (cape-company-to-capf #'company-R-library)
-              ;; (cape-company-to-capf #'company-R-args)
-                      ;; #'cape-dabbrev
-                      ;; #'ess-r-object-completion
-                      ;; #'ess-r-package-completion
+              (cape-company-to-capf #'company-R-objects)
+              (cape-company-to-capf #'company-R-library)
+              (cape-company-to-capf #'company-R-args)
+                      #'cape-dabbrev
+                      #'ess-r-object-completion
+                      #'ess-r-package-completion
                       )
-             ;; #'eglot-completion-at-point
+             #'eglot-completion-at-point
              ))
     ))
 
@@ -1943,13 +1882,8 @@ DIR must include a .project file to be considered a project."
   :custom
   (eglot-sync-connect nil)
   :config
-  ;; Better defaults for eglot
-  ;; may be bad for troubleshooting issues with LSP
-  ;; https://old.reddit.com/r/emacs/comments/17jrsmv/share_how_did_you_make_emacs_faster/k74b3tg/
-  (advice-add 'jsonrpc--log-event :override #'ignore)
-  (setopt eglot-events-buffer-size 0)
   (setq completion-category-overrides '((eglot (styles orderless))))
-  ;; (add-hook 'ess-r-mode-hook #'eglot-ensure)
+  (add-hook 'ess-r-mode-hook #'eglot-ensure)
   (add-to-list 'eglot-server-programs '(markdown-mode . ("marksman")))
   (add-hook 'python-mode #'eglot-ensure)
   (setq eglot-stay-out-of '(company cape corfu))
@@ -1958,7 +1892,7 @@ DIR must include a .project file to be considered a project."
     (setq eldoc-documentation-strategy
             'eldoc-documentation-compose-eagerly))
   :hook ((eglot-managed-mode . mp-eglot-eldoc)
-     ;; (ess-mode . eglot-ensure)
+     (ess-mode . eglot-ensure)
      (inferior-ess-mode . eglot-ensure)
      (julia-mode . eglot-ensure)
      (python-mode . eglot-ensure)
@@ -1993,7 +1927,7 @@ DIR must include a .project file to be considered a project."
 
 (use-package flycheck
   :straight t
-  ;; :defer 80
+  :defer 80
   :preface
   (defun mp-flycheck-eldoc (callback &rest _ignored)
     "Print flycheck messages at point by calling CALLBACK."
