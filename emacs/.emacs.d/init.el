3a4287812ef30000e29616f6195945334898a201
paskn <pashakhin@gmail.com>
2025-10-16 18:59:42 +0200

    Deprecate org-roam

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1337,28 +1337,6 @@ DIR must include a .project file to be considered a project."
   :config
   (consult-denote-mode 1))
 
-(use-package org-roam
-  :straight t
-  :defer t
-  :init
-  :custom
-  (org-roam-directory "~/Documents/personal/RoamNotes")
-  (org-roam-completion-everywhere t)
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ;; ("c-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         :map org-mode-map
-         ("C-M-c" . completion-at-point)
-         :map org-roam-dailies-map
-         ("Y" . org-roam-dailies-capture-yesterday)
-         ("T" . org-roam-dailies-capture-tomorrow))
-  :bind-keymap
-  ("C-c n d" . org-roam-dailies-map)
-  :config
-  (require 'org-roam-dailies) ;; Ensure the keymap is available
-  (org-roam-db-autosync-mode 1))
-
 (use-package mastodon
   :straight t
   :defer t
