e56b10f944b4836eac4261da0cf9a5c6225d2847
paskn <pashakhin@gmail.com>
2026-05-30 15:37:38 +0200

    Add more intuitive C-g dwim
    from https://protesilaos.com/codelog/2026-04-30-emacs-decent-default-sacha-chua/

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -445,6 +445,31 @@ group by projectile projects.")
 ;; source: https://protesilaos.com/codelog/2020-07-16-emacs-focused-editing/
 (use-package emacs
   :straight nil
+  :init
+  (defun prot/keyboard-quit-dwim ()
+    "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+
+The generic `keyboard-quit' does not do the expected thing when
+the minibuffer is open.  Whereas we want it to close the
+minibuffer, even without explicitly focusing it.
+
+The DWIM behaviour of this command is as follows:
+
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+    (interactive)
+    (cond
+     ((region-active-p)
+      (keyboard-quit))
+     ((derived-mode-p 'completion-list-mode)
+      (delete-completion-window))
+     ((> (minibuffer-depth) 0)
+      (abort-recursive-edit))
+     (t
+      (keyboard-quit))))
+  :bind ("C-g" . prot/keyboard-quit-dwim)
   :config
   ;; (pixel-scroll-precision-mode 1)
   (setq show-paren-delay 0)
